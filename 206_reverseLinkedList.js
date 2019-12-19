c065545570e827b1e9b9e8bab3e94aa8e862dfc8
David Rider <riderda@oregonstate.edu>
2019-12-19 21:37:30 +0900

    Add files via upload

---
+++ 206_reverseLinkedList.js
@@ -0,0 +1,20 @@
+function ListNode(val) {
+    this.val = val;
+    this.next = null;
+}
+ 
+var reverseList = function(head) {
+    let prev = null;
+    let cur = head;
+    let next = null;
+
+    while (cur != null) {
+    	next = cur.next;
+    	cur.next = prev;
+    	prev = cur;
+    	cur = next;
+    }
+
+    return prev;
+
+};
